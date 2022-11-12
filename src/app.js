7df5378dff41a537613d5f57cf44a0bacb4fd5f5
Wayne Nacle <100382524+WayneNtkM@users.noreply.github.com>
2022-11-11 22:12:03 -0300

    Config APP.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const { productRouter, salesRouter } = require('./routes/index');
 
 const app = express();
+app.use(express.json());
+
+app.use('/products', productRouter);
+app.use('/sales', salesRouter);
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
